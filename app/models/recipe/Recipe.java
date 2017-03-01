ed6f7fb855a63a62dc4eeaeaa7a70697bcf4a78d
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-03-01 15:26:38 +0100

    added total nutrient methods to recipe

--- app/models/recipe/Recipe.java
+++ app/models/recipe/Recipe.java
@@ -9,6 +9,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
 import java.util.Set;
+import java.util.function.Function;
 
 @Entity
 @Table(name = "Recipes")
@@ -23,7 +24,7 @@ public class Recipe extends Model {
 
     public Set<Ingredient> ingredients;
 
-    @Column private String sourceUrl;
+    public String sourceUrl;
 
     public Recipe(String title, String description, int portions, Set<Ingredient> ingredients) {
         this.title = title;
@@ -33,4 +34,38 @@ public class Recipe extends Model {
     }
 
     public static Finder<Long, Recipe> find = new Finder<>(Recipe.class);
+
+    public Double getEnergyKcal() {
+        return ingredients.stream().mapToDouble(Ingredient::getEnergyKcal).sum();
+    }
+    public Double getEnergyKj() {
+        return ingredients.stream().mapToDouble(Ingredient::getEnergyKj).sum();
+    }
+    public Double getCarbohydrates() {
+        return ingredients.stream().mapToDouble(Ingredient::getCarbohydrates).sum();
+    }
+    public Double getProtein() {
+        return ingredients.stream().mapToDouble(Ingredient::getProtein).sum();
+    }
+    public Double getFibre() {
+        return ingredients.stream().mapToDouble(Ingredient::getFibre).sum();
+    }
+    public Double getWholeGrain() {
+        return ingredients.stream().mapToDouble(Ingredient::getWholeGrain).sum();
+    }
+    public Double getCholesterol() {
+        return ingredients.stream().mapToDouble(Ingredient::getCholesterol).sum();
+    }
+    public Double getWater() {
+        return ingredients.stream().mapToDouble(Ingredient::getWater).sum();
+    }
+    public Double getAlcohol() {
+        return ingredients.stream().mapToDouble(Ingredient::getAlcohol).sum();
+    }
+    public Double getAsh() {
+        return ingredients.stream().mapToDouble(Ingredient::getAsh).sum();
+    }
+    public Double getWaste() {
+        return ingredients.stream().mapToDouble(Ingredient::getWaste).sum();
+    }
 }
