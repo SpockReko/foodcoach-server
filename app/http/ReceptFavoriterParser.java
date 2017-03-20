e4db647af9cc373793e2f0ead756fad606f9f0f9
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-03-20 13:11:45 +0100

    found ingredient strings

--- app/http/ReceptFavoriterParser.java
+++ app/http/ReceptFavoriterParser.java
@@ -3,6 +3,8 @@ package http;
 import models.recipe.Recipe;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Created by fredrikkindstrom on 2017-03-20.
@@ -14,7 +16,12 @@ public class ReceptFavoriterParser implements IRecipeParser {
         Document doc = Jsoup.parse(html);
 
         String title = doc.select("h1[itemprop=name]").text();
-        
+
+        Elements ingredients = doc.select(".recipe-ingredients li");
+
+        for (Element ingredient : ingredients) {
+            System.out.println(ingredient.text());
+        }
         return new Recipe(title, 4, null);
     }
 }
