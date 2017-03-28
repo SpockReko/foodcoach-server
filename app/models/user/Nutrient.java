47f96988dd473c9bb901369e86a62c8f2e87f5b5
Loiiiz <rost.louise@gmail.com>
2017-03-28 16:03:27 +0200

    added overdose function

---
+++ app/models/user/Nutrient.java
@@ -0,0 +1,31 @@
+package models.user;
+
+/**
+ * Created by Admin on 2017-03-23.
+ */
+public enum Nutrient {
+        CaloriKcal,
+        Protein,
+        Carbohydrates,
+        Fat,
+        Fibre,
+        VitaminAUG,
+        VitaminDUG,
+        VitaminEMG,
+        ThiamineMG,
+        RiboflavinMG,
+        NiacinMG,
+        VitaminB6MG,
+        FolateUG,
+        VitaminB12UG,
+        VitaminCMG,
+        CalciumMG,
+        PhosphorusMG,
+        PotassiumMG,
+        MagnesiumMG,
+        IronMG,
+        ZinkMG,
+        CopperMG,
+        IodineUG,
+        SeleniumUG
+}
