de262d9e44dcd4ced3e203d84e71634c926de0a5
Loiiiz <rost.louise@gmail.com>
2017-04-07 10:42:50 +0200

    additions to recipe optimization

--- app/controllers/RecipeOptimizationController.java
+++ app/controllers/RecipeOptimizationController.java
@@ -11,8 +11,20 @@ import play.mvc.Result;
  */
 public class RecipeOptimizationController extends Controller {
 
-    public Result recipeOptimization() {
-        Recipe recipe = Recipe.find.byId(1L);
+    // GET /recipe/:id
+    public Result optimizeByNumber(long recipeNumber) {
+        Recipe recipe = Recipe.find.byId(recipeNumber);
+        User user = new User();
+        RecipeOptimizer recipeOptimizerInstant = new RecipeOptimizer(recipe, user);
+        recipeOptimizerInstant.setLowestPercentageOfIngredient(0.75D);
+        Recipe optimizedRecipe = recipeOptimizerInstant.optimizeRecipe();
+
+        return ok(recipeOptimizerInstant.recipeToString(optimizedRecipe));
+    }
+
+    // GET /recipe/title/:title
+    public Result optimizeByTitle(String title) {
+        Recipe recipe = Recipe.find.where().eq("title", title).findUnique();
         User user = new User();
         RecipeOptimizer recipeOptimizerInstant = new RecipeOptimizer(recipe, user);
         recipeOptimizerInstant.setLowestPercentageOfIngredient(0.75D);
