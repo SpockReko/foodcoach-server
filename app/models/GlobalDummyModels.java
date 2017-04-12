36c946769fda26cb403475347e0f7a7831adc694
stefan <stefan.fritzon@gmail.com>
2017-04-12 15:26:46 +0200

    Changed the food name in dummys

--- app/models/GlobalDummyModels.java
+++ app/models/GlobalDummyModels.java
@@ -68,37 +68,37 @@ public class GlobalDummyModels {
 
     // Nutrient taken from the finish food database
     public static Food getFoodDummyCarrot() {
-        return new Food("Morot", 300, 317.0, 8.1, 0.6, 0.2, 2.5, 0.0, 0.0,
+        return new Food("Carrot", 300, 317.0, 8.1, 0.6, 0.2, 2.5, 0.0, 0.0,
                 774.12, 0.03, 0.0, 6.5, 0.0, 0.36, 19.0, 0.07, 0.07, 0.8,
                 0.9, 16.4, 40.0, 1.0, 0.5, 29.0 , 390.0, 14.0, 25.0, 0.2 , 0.4 );
     }
 
     public static Food getFoodDummyParsnip() {
-        return new Food("Palsternacka", 309, 266.0, 11.8, 0.5, 0.4, 4.5, 0.0, 2.0,
+        return new Food("Parsnips", 309, 266.0, 11.8, 0.5, 0.4, 4.5, 0.0, 2.0,
                 0.3, 0.03, 0.0, 6.5, 0.0, 0.4, 19.0, 0.07, 0.07, 0.8,
                 0.9, 16.4, 40.0, 1.0, 0.5, 29.0, 390.0, 14.0, 25.0, 0.2, 0.4);
     }
 
     public static Food getFoodDummyChicken() {
-        return new Food("Kycklingbröst", 11565, 456.0, 0.0, 22.4, 2.0, 0.0, 0.0, 86.6,
+        return new Food("Chicken", 11565, 456.0, 0.0, 22.4, 2.0, 0.0, 0.0, 86.6,
                 11.0, 0.51, 1.0, 0.0, 0.1, 0.7, 15.0, 0.14, 0.18, 7.8,
                 11.8, 15.6, 160.0, 6.0, 0.4, 6.0, 380.0, 26.0, 34.0, 24.0, 0.7);
     }
 
     public static Food getFoodDummyBean() {
-        return new Food("Bruna/vita bönor", 373, 464.7, 14.0, 8.3, 0.8, 7.0, 0.0, 12.74,
+        return new Food("Beans", 373, 464.7, 14.0, 8.3, 0.8, 7.0, 0.0, 12.74,
                 39.960, 0.18, 0.0, 0.0, 0.0, 0.129, 27.0, 0.12, 0.05, 0.7,
                 2.223, 167.58, 147.9, 0.0, 1.886, 81.66, 364.97, 48.910, 5.00, 0.390, 0.81);
     }
 
     public static Food getFoodDummyRedLenses() {
-        return new Food("Röda Linser", 31225, 424.4, 15.6, 7.6, 0.4, 1.9, 0.0, 30.58, 20.0,
+        return new Food("Red Lenses", 31225, 424.4, 15.6, 7.6, 0.4, 1.9, 0.0, 30.58, 20.0,
                 0.11, 0.0, 0.0, 0.0, 0.11, 1.7, 0.11, 0.04, 0.4,
                 1.402, 5.00, 100.0, 0.0, 2.400, 16.00, 220.00, 220.00, 12.00, 2.00, 1.00);
     }
 
     public static Food getFoodDummySteak() {
-        return new Food("Nötkött, ", 31145, 795.43, 1.4, 22.19, 14.375, 0.0, 0.0, 1054.51,
+        return new Food("Beef, ", 31145, 795.43, 1.4, 22.19, 14.375, 0.0, 0.0, 1054.51,
                 3.163, 0.11, 0.91, 0.0, 0.13, 2.555, 21.396, 0.0715, 0.11, 3.7,
                 7.179, 0.76, 137.3, 2.05, 2.020, 7.50, 260.42, 260.42, 413.86, 10.415, 2.471);
     }
