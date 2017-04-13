e899c7b3f71c60d1d1ed11c55f9b19ae74324419
stefan <stefan.fritzon@gmail.com>
2017-04-13 13:58:43 +0200

    Now we have a dynamic method and can get more than one weekmenu

--- app/algorithms/MenuAlgorithms.java
+++ app/algorithms/MenuAlgorithms.java
@@ -22,6 +22,7 @@ public class MenuAlgorithms {
     private final Double LARGE_DISTANCE = 999999999.9999;
     private Double optimalMenuNutrition;
     private Menu optimalMenu = new Menu(new ArrayList<>());
+    private Menu[] menus;
     private int nrOfRecipes;
     private List<Recipe> allRecipes = new ArrayList<>();
     private List<Menu> weekMenuList = new ArrayList<>();
@@ -32,6 +33,17 @@ public class MenuAlgorithms {
     public MenuAlgorithms(User user, List<Recipe> recipeList){
         this.user = user;
         this.allRecipes = recipeList;
+        menus = new Menu[1];
+    }
+
+    public MenuAlgorithms(User user, List<Recipe> recipeList, int numberOfMenus){
+        this.user = user;
+        this.allRecipes = recipeList;
+        menus = new Menu[numberOfMenus];  //An extenstion to have more than one menu.
+        for (Menu m: menus) {
+            m = null;
+        }
+
     }
 
     private void reset(){
@@ -47,17 +59,20 @@ public class MenuAlgorithms {
     /**
      * Recursiv method with 2 base cases and a minimization of the value.
      * In the case we get a size that is good then we can
-     * @param indexOfRecipes
-     * @param currentList
+     * @param indexOfRecipes where in the recurtion we ar at
+     * @param currentList the list we have at each recurtion
+     * @param optimize The method we want to evaluate the menu on
      * @return
      */
-    public double returnAllWeekMenus(
-            int indexOfRecipes, List<Recipe> currentList, Function<Menu,Double> optimize){
+    public double returnAllWeekMenus(int indexOfRecipes, List<Recipe> currentList, Function<Menu,Double> optimize){
 
         if (currentList.size() == nrOfRecipes){
             Menu menu = new Menu(currentList);
             double value = optimize.apply(menu);
-            if(value <= optimalMenuNutrition) {
+            int optimizeMenuIndex = getOptimalMenuIndex(optimize);
+            Menu optimizeMenu = menus[optimizeMenuIndex] == null ? menu : menus[optimizeMenuIndex];
+            if(value <= optimize.apply(optimizeMenu)) {
+
                 optimalMenuNutrition = value;
                 optimalMenu = menu;
             }
@@ -73,10 +88,36 @@ public class MenuAlgorithms {
 
     }
 
+    /**
+     * returns the least optimized value in menus
+     * @param optimize
+     * @return menu;
+     */
+    private int getOptimalMenuIndex(Function<Menu, Double> optimize) {
+        Menu menu = menus[0];
+        int returnIndex = 0;
+        if( menu == null) return 0;
+
+        for (int i = 0; i < menus.length; i++) {
+            if(    menus[i] == null) return i;
+
+            double mValue = optimize.apply(menus[i]);
+            double menuValue = optimize.apply(menu);
+
+            if(mValue>menuValue){ //The worse value is better!
+                returnIndex = i;
+            }
+        }
+        return returnIndex;
+    }
+
     public Menu calculateWeekMenu(List<Recipe> notThisRecipes) {
         reset();
         filterRecipes(notThisIngredients,notThisRecipes);
-        returnAllWeekMenus(allRecipes.size()-1,new ArrayList<>(),this::nutritionValueCalculation);
+        returnAllWeekMenus(
+                allRecipes.size()-1,
+                new ArrayList<>(),
+                this::nutritionValueCalculation);
         return optimalMenu;
     }
 
