632df289b235e05de91d0efab9af76c2125f354e
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-04-20 13:15:47 +0200

    added Food files

---
+++ app/models/food/DataSource.java
@@ -0,0 +1,8 @@
+package models.food;
+
+/**
+ * Created by fredrikkindstrom on 2017-04-04.
+ */
+public enum DataSource {
+    FINELI, LMV
+}
---
+++ app/models/food/Diet.java
@@ -0,0 +1,27 @@
+package models.food;
+
+import javax.persistence.*;
+
+/**
+ * Represents a special diet that a food can belong to.
+ *
+ * @author Fredrik Kindstrom
+ */
+@Entity
+@Table(name = "Diets")
+public class Diet {
+
+    @Id private long id;
+    @Enumerated(EnumType.STRING) public Type type;
+
+    public Diet(Type type) {
+        this.type = type;
+    }
+
+    public enum Type {
+        CHOLESTEROL_FREE, EGG_FREE, FAT_FREE, GLUTEN_FREE, HIGH_FIBRE, STRONGLY_SALTED,
+        LACTO_OVO_VEG, LACTO_VEG, LACTOSE_FREE, LOW_CHOLESTEROL, LOW_FAT, LOW_LACTOSE,
+        LOW_PROTEIN, REDUCED_SALT, MILK_FREE, NATURALLY_GLUTEN_FREE, SALT_FREE, SOY_FREE,
+        UNSWEETENED, VEGAN, ADDED_VITAMINS
+    }
+}
