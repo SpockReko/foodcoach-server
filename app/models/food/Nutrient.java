37ad3308908e8e97d77e787ef0e6c59eaff8fd02
Loiiiz <rost.louise@gmail.com>
2017-04-25 14:31:14 +0200

    Added names in Nutrient

--- app/models/food/Nutrient.java
+++ app/models/food/Nutrient.java
@@ -7,46 +7,52 @@ package models.food;
  * @author Fredrik Kindstrom
  */
 public enum Nutrient {
-    ENERGY_KCAL(Type.OTHER, Unit.OTHER),
-    ENERGY_KJ(Type.OTHER, Unit.OTHER),
-    CARBOHYDRATES(Type.SUGAR, Unit.GRAMS),
-    PROTEIN(Type.PROTEIN, Unit.GRAMS),
-    FAT(Type.FAT, Unit.GRAMS),
-    FIBRE(Type.OTHER, Unit.GRAMS),
-    SALT(Type.MINERAL, Unit.GRAMS),
-    ALCOHOL(Type.OTHER, Unit.GRAMS),
+    ENERGY_KCAL("Energi", Type.OTHER, Unit.OTHER),
+    ENERGY_KJ("Energi", Type.OTHER, Unit.OTHER),
+    CARBOHYDRATES("Kolhydrater", Type.SUGAR, Unit.GRAMS),
+    PROTEIN("Protein", Type.PROTEIN, Unit.GRAMS),
+    FAT("Fett", Type.FAT, Unit.GRAMS),
+    FIBRE("Fibrer", Type.OTHER, Unit.GRAMS),
+    SALT("Salt", Type.MINERAL, Unit.GRAMS),
+    ALCOHOL("Alkohol", Type.OTHER, Unit.GRAMS),
 
-    VITAMIN_A(Type.VITAMIN, Unit.MICROGRAMS),
-    VITAMIN_B6(Type.VITAMIN, Unit.MILLIGRAMS),
-    VITAMIN_B12(Type.VITAMIN, Unit.MICROGRAMS),
-    VITAMIN_C(Type.VITAMIN, Unit.MILLIGRAMS),
-    VITAMIN_D(Type.VITAMIN, Unit.MICROGRAMS),
-    VITAMIN_E(Type.VITAMIN, Unit.MILLIGRAMS),
-    VITAMIN_K(Type.VITAMIN, Unit.MICROGRAMS),
-    THIAMINE(Type.VITAMIN, Unit.MILLIGRAMS),
-    RIBOFLAVIN(Type.VITAMIN, Unit.MILLIGRAMS),
-    NIACIN(Type.VITAMIN, Unit.MILLIGRAMS),
-    NIACIN_EQ(Type.VITAMIN, Unit.MILLIGRAMS),
+    VITAMIN_A("Vitamin A", Type.VITAMIN, Unit.MICROGRAMS),
+    VITAMIN_B6("Vitamin B6", Type.VITAMIN, Unit.MILLIGRAMS),
+    VITAMIN_B12("Vitamin B12", Type.VITAMIN, Unit.MICROGRAMS),
+    VITAMIN_C("Vitamin C", Type.VITAMIN, Unit.MILLIGRAMS),
+    VITAMIN_D("Vitamin D", Type.VITAMIN, Unit.MICROGRAMS),
+    VITAMIN_E("Vitamin E", Type.VITAMIN, Unit.MILLIGRAMS),
+    VITAMIN_K("Vitamin K", Type.VITAMIN, Unit.MICROGRAMS),
+    THIAMINE("Tiamin", Type.VITAMIN, Unit.MILLIGRAMS),
+    RIBOFLAVIN("Riboflavin", Type.VITAMIN, Unit.MILLIGRAMS),
+    NIACIN("Niacin", Type.VITAMIN, Unit.MILLIGRAMS),
+    NIACIN_EQ("Niacinekvivalenter", Type.VITAMIN, Unit.MILLIGRAMS),
 
-    FOLATE(Type.MINERAL, Unit.MICROGRAMS),
-    PHOSPHORUS(Type.MINERAL, Unit.MILLIGRAMS),
-    IODINE(Type.MINERAL, Unit.MICROGRAMS),
-    IRON(Type.MINERAL, Unit.MILLIGRAMS),
-    CALCIUM(Type.MINERAL, Unit.MILLIGRAMS),
-    POTASSIUM(Type.MINERAL, Unit.MILLIGRAMS),
-    MAGNESIUM(Type.MINERAL, Unit.MILLIGRAMS),
-    SODIUM(Type.MINERAL, Unit.MILLIGRAMS),
-    SELENIUM(Type.MINERAL, Unit.MICROGRAMS),
-    ZINC(Type.MINERAL, Unit.MILLIGRAMS);
+    FOLATE("Folat", Type.MINERAL, Unit.MICROGRAMS),
+    PHOSPHORUS("Fosfor", Type.MINERAL, Unit.MILLIGRAMS),
+    IODINE("Jod", Type.MINERAL, Unit.MICROGRAMS),
+    IRON("Järn", Type.MINERAL, Unit.MILLIGRAMS),
+    CALCIUM("Kalcium", Type.MINERAL, Unit.MILLIGRAMS),
+    POTASSIUM("Kalium", Type.MINERAL, Unit.MILLIGRAMS),
+    MAGNESIUM("Magnesium", Type.MINERAL, Unit.MILLIGRAMS),
+    SODIUM("Natrium", Type.MINERAL, Unit.MILLIGRAMS),
+    SELENIUM("Selenium", Type.MINERAL, Unit.MICROGRAMS),
+    ZINC("Zink", Type.MINERAL, Unit.MILLIGRAMS);
 
+    private final String name;
     private final Type type;
     private final Unit unit;
 
-    Nutrient(Type type, Unit unit) {
+    Nutrient(String name, Type type, Unit unit) {
+        this.name = name;
         this.type = type;
         this.unit = unit;
     }
 
     public enum Type { SUGAR, FAT, VITAMIN, MINERAL, PROTEIN, OTHER }
     public enum Unit { GRAMS, MILLIGRAMS, MICROGRAMS, OTHER }
+
+    public String toString() {
+        return this.name;
+    }
 }
